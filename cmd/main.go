1214831d9cec7f77a444b27a466197188b773f0a
einarorn <einarorn79@gmail.com>
2024-01-08 12:44:04 +0000

    add more strucutre

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,14 @@ import (
 	"log"
 	"net/http"
 	"text/template"
-)
 
-type Automobile struct {
-	Make  string
-	Model string
-	Year  int
-}
+	"github.com/einarorn/go-htmx/model"
+)
 
 func main() {
 	indexHandler := func(w http.ResponseWriter, req *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		autos := map[string][]Automobile{
+		tmpl := template.Must(template.ParseFiles("view/index.html"))
+		autos := map[string][]model.Automobile{
 			"Autos": {
 				{Make: "Mercedes", Model: "C300e", Year: 2020},
 				{Make: "Volvo", Model: "XC90 T8", Year: 2016},
---
+++ model/automobile.go
@@ -0,0 +1,7 @@
+package model
+
+type Automobile struct {
+	Make  string
+	Model string
+	Year  int
+}
